a6eab5ee2d79217ce2a9036cfaefd90b08e9e674
noorulqumar <noorulqumar0@gmail.com>
2023-11-15 19:04:37 +0500

    file deleted

---
+++ script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -x
+eval "$(ssh-agent -s)"
+ssh-add - <<< "$PRIVATE_KEY"
+ssh -o StrictHostKeyChecking=no ubuntu@51.20.144.90 
+echo 'SSH connection successful'
+if command -v docker &>/dev/null; then
+    echo "Docker is installed."
+else
+    echo "Docker is not installed."
+    sudo apt update -y
+    sudo apt install docker.io -y
+    sudo usermod -aG docker "$USER"
+    echo "$USER"
+    newgrp docker
+    sleep 10
+    sudo systemctl start docker
+fi
+
+docker images
+if command -v awscli &>/dev/null
+then
+    if (( $? !=  0 ))
+    then
+        echo "installing AWSCLI"
+        sudo apt update
+        sudo apt  install awscli -y
+    else
+        echo "awscli is installed"
+    fi
+fi
+aws configure set aws_access_key_id "$ACCESS_KEY"
+aws configure set aws_secret_access_key "$SECRET_KEY"
+aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 294894169523.dkr.ecr.eu-north-1.amazonaws.com
+docker rm -f "$(docker ps -aq)"
+docker run -d -p 80:80 294894169523.dkr.ecr.eu-north-1.amazonaws.com/test-repo-1:2
